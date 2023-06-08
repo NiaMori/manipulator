69e2035ca3fcbf53541fe69b247d402799d98d27
NiaMori <Hellc5261@gmail.com>
2023-06-08 10:20:40 +0800

    refactor: seperate manipulate test utils

--- test/manipulate/manipulate.test.ts
+++ test/manipulate/manipulate.test.ts
@@ -1,35 +1,6 @@
 import fs from 'node:fs/promises'
 import { describe, expect, it } from 'vitest'
-import type { Draft } from 'immer'
-import { produce } from 'immer'
-import { $ } from 'zx'
-import { z } from 'zod'
-import { manipulateJson } from '@'
-
-expect.addSnapshotSerializer({
-  test: value => typeof value === 'string',
-  serialize: string => string,
-})
-
-async function diff(aString: string, bString: string): Promise<string> {
-  try {
-    await $`diff -U0 --label OLD_JSON <(echo ${aString}) --label NEW_JSON <(echo ${bString})`.quiet()
-    return ''
-  } catch (e) {
-    return z.object({
-      stdout: z.string(),
-    }).parse(e).stdout
-  }
-}
-
-async function kase<T = any>(json: string, recipe: (dr: Draft<T>) => void): Promise<string> {
-  const newValue = produce(JSON.parse(json), recipe)
-  const newJson = manipulateJson<T>(json, recipe)
-
-  expect(JSON.parse(newJson)).toEqual(newValue)
-
-  return await diff(json, newJson)
-}
+import { kase } from './manipulate.utils'
 
 describe('manipulate', () => {
   // TODO: add more tests besides the sanity check
---
+++ test/manipulate/manipulate.utils.ts
@@ -0,0 +1,31 @@
+import { expect } from 'vitest'
+import type { Draft } from 'immer'
+import { produce } from 'immer'
+import { $ } from 'zx'
+import { z } from 'zod'
+import { manipulateJson } from '@'
+
+expect.addSnapshotSerializer({
+  test: value => typeof value === 'string',
+  serialize: string => string,
+})
+
+async function diff(aString: string, bString: string): Promise<string> {
+  try {
+    await $`diff -U0 --label OLD_JSON <(echo ${aString}) --label NEW_JSON <(echo ${bString})`.quiet()
+    return ''
+  } catch (e) {
+    return z.object({
+      stdout: z.string(),
+    }).parse(e).stdout
+  }
+}
+
+export async function kase<T = any>(json: string, recipe: (dr: Draft<T>) => void): Promise<string> {
+  const newValue = produce<T>(JSON.parse(json), recipe)
+  const newJson = manipulateJson<T>(json, recipe)
+
+  expect(JSON.parse(newJson)).toEqual(JSON.parse(JSON.stringify(newValue)))
+
+  return await diff(json, newJson)
+}
